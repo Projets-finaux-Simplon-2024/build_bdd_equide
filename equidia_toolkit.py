f9061e00c7fa3df52194beaba7260de081683b12
JeremyMikaleff <jeremy.mikaleff@gmail.com>
2024-04-29 16:48:42 +0200

    fix(build_bdd_equide): changement de tkinter vers flask + pywebviewer pour l'affichage

--- equidia_toolkit.py
+++ equidia_toolkit.py
@@ -1,54 +1,50 @@
-import tkinter as tk
-from tkinter_template.loading import LoadingFrame
-from tkinter_template.aframe_factory import get_frame
+from flask import Flask, render_template
+import webview
+import threading
+import os
 
-#-------------------------------------------------------- BASE APPLICATION --------------------------------------------------------
-class App(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("Application de Scraping Equidia")
-        self.setup_window()
-        self.current_frame = None
-        self.loading_frame = LoadingFrame(self)
-        self.frames = {}  # Cache for frame instances
+app = Flask(__name__)
 
-        self.show_frame('HomeFrame')  # Use string identifiers
 
-    def setup_window(self):
-        screen_width = self.winfo_screenwidth()
-        screen_height = self.winfo_screenheight()
-        print(screen_width, screen_height)
-
-        window_width = int((screen_width * 80) / 100)
-        window_height = int((screen_height * 80) / 100)
-
-        center_x = int(screen_width / 2 - window_width / 2)
-        center_y = int(screen_height / 2 - window_height / 2)
-        self.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
-
-    def show_frame(self, frame_name):
-        if self.current_frame is not None:
-            self.current_frame.grid_remove()
-        self.loading_frame.show_loading()  # Affiche la page de chargement
-        self.after(100, lambda: self.load_frame(frame_name))  # Temporisation pour simuler un chargement
-
-    def load_frame(self, frame_name):
-        if frame_name not in self.frames:
-            # Request a new frame from the factory if not cached
-            self.frames[frame_name] = get_frame(frame_name, self)
-        self.current_frame = self.frames[frame_name]
-        self.current_frame.grid(row=0, column=0, sticky="nsew")
-        self.loading_frame.hide_loading()  # Cache la page de chargement après le chargement
-#----------------------------------------------------------------------------------------------------------------------------------
+# |---------------------------------------------------------- PAGE ACCUEIL --------------------------------------------------------------------|
+@app.route("/")
+def index():
+    # Notez le chemin mis à jour pour inclure le sous-dossier
+    return render_template('index.html')
+# |--------------------------------------------------------------------------------------------------------------------------------------------|
 
 
 
 
-#-------------------------------------------------------- MAIN --------------------------------------------------------------------
+
+
+# |---------------------------------------------------------- PAGE DE SCRAPING DE BASE --------------------------------------------------------|
+@app.route("/second-page")
+def second_page():
+    # De même, mettez à jour le chemin ici
+    return render_template('scraping_chevaux_bases.html')
+# |--------------------------------------------------------------------------------------------------------------------------------------------|
+
+
+
+
+
+
+# |---------------------------------------------------------- CREATION DE LA FENËTRE ----------------------------------------------------------|
+def flask_thread():
+    app.run(use_reloader=False, port=5000)
+
 if __name__ == "__main__":
-    print("Starting application...")
-    app = App()
-    print("Application is running.")
-    app.mainloop()
-    print("Application closed.")  # Debug: confirm application closure
-#----------------------------------------------------------------------------------------------------------------------------------
+    # Créer un thread pour l'application Flask
+    flask_app_thread = threading.Thread(target=flask_thread)
+    flask_app_thread.start()
+
+    # Créer et afficher la fenêtre PyWebView
+    webview.create_window("Mon Application Flask", "http://127.0.0.1:5000")
+
+    # Attendre que la fenêtre soit complètement fermée
+    webview.start()
+
+    # Arrêter le serveur Flask une fois la fenêtre fermée
+    os._exit(0)  # Utiliser os._exit() pour terminer tous les threads et le processus principal
+# |-------------------------------------------------------------------------------------------------------------------------------------------|
---
+++ tools_scraping/recup_annees.py
@@ -0,0 +1,24 @@
+from connexions_files.con_ifce import connexion_ifce
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By    
+
+def recup_annees():
+        driver = connexion_ifce()
+        try:
+            wait = WebDriverWait(driver, 10)
+            
+            # Cliquer sur le bouton pour dérouler la liste des années si nécessaire
+            toggle_button = wait.until(EC.element_to_be_clickable((By.ID, "reproducteur-annee_naissance_cheval")))
+            toggle_button.click()
+            
+            # Attendre que la liste soit visible et récupérer les éléments LI
+            wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "ul[aria-labelledby='reproducteur-annee_naissance_cheval'] li")))
+            li_elements = driver.find_elements(By.CSS_SELECTOR, "ul[aria-labelledby='reproducteur-annee_naissance_cheval'] li")
+            
+            # Extraire le texte (les années) de chaque élément LI
+            years = [li.text.split()[0] for li in li_elements]  # Utiliser split() si nécessaire pour nettoyer les données
+            filtered_years = [year for year in years if int(year) >= 1950]
+            return filtered_years
+        finally:
+            driver.quit()
