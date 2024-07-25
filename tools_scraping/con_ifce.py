f353e726db217d6e83db5a7c3467f9b9a5efccbc
JeremyMikaleff <jeremy.mikaleff@gmail.com>
2024-07-25 13:30:25 +0200

    fix(build_bdd_equide): fix les bugs de Chromedriver

--- tools_scraping/con_ifce.py
+++ tools_scraping/con_ifce.py
@@ -3,17 +3,26 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 # Configuration du pilote Selenium et ouverture du navigateur ---------------------------------------------------------------------------------------
 # Configurer les options Chrome pour le mode headless
-def connexion_ifce() :
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--window-size=1920,1080")  # Simuler une taille de fenêtre standard
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+# Chemin absolu vers le fichier chromedriver.exe
+CHROME_DRIVER = "D:/aProjects/projet_equide/build_bdd_equide/chromedriver-win64/chromedriver.exe"
 
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
-    url = 'https://infochevaux.ifce.fr/fr/info-chevaux?utm_source=Effiweb&utm_medium=Menu%20SIRE%20Demarches&utm_campaign=SIRE%20%E2%80%93%20Infochevaux'
-    driver.get(url)
-    return(driver)
+def connexion_ifce():
+    try:
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+
+        # Utiliser le chemin absolu vers chromedriver.exe
+        service = ChromeService(executable_path=CHROME_DRIVER)
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        
+        url = 'https://infochevaux.ifce.fr/fr/info-chevaux?utm_source=Effiweb&utm_medium=Menu%20SIRE%20Demarches&utm_campaign=SIRE%20%E2%80%93%20Infochevaux'
+        driver.get(url)
+        return driver
+    except Exception as e:
+        print(f"Une erreur s'est produite lors de la connexion : {e}")
+        return None
 #----------------------------------------------------------------------------------------------------------------------------------------------------
